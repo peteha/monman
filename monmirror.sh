d94bcd018bda66e3edc7ea0bc6bdf1755756516e
Peter Hauck <peteha@heartofgold.local>
2022-02-04 04:47:24 +0000

    Updated with paths.

--- monmirror.sh
+++ monmirror.sh
@@ -1,8 +1,8 @@
-if mirror -q | grep -q 'on'; then 
-  mirror -off
+if /opt/homebrew/bin/mirror -q | grep -q 'on'; then 
+  /opt/homebrew/bin/mirror -off
   exit
 fi
-if mirror -q | grep -q 'off'; then
-  mirror -l 1 0
+if /opt/homebrew/bin/mirror -q | grep -q 'off'; then
+  /opt/homebrew/bin/mirror -l 1 0
   exit
 fi
